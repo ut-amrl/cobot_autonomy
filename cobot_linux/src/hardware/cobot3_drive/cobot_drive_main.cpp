a248718a1d7fffa29a4818ef531f1311f0c482ee
tiejean <taijingchen99@gmail.com>
2025-07-02 11:49:43 -0500

    fix imports for cobot_drive_main.cpp

--- cobot_linux/src/hardware/cobot3_drive/cobot_drive_main.cpp
+++ cobot_linux/src/hardware/cobot3_drive/cobot_drive_main.cpp
@@ -8,6 +8,15 @@
 #include "proghelp.h"           
 #include "configreader.h"
 
+#include <rclcpp/rclcpp.hpp>
+#include <ament_index_cpp/get_package_share_directory.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+#include "cobot_msgs/msg/cobot_drive_msg.hpp"
+#include "cobot_msgs/msg/cobot_odometry_msg.hpp"
+
 int main(int argc, char **argv) {
     std::cout << "Cobot Drive Main Program" << std::endl;
 }
