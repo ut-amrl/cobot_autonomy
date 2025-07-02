7eb0d45287e76740c4364a9a4af3e2d7761e44c3
tiejean <taijingchen99@gmail.com>
2025-07-02 09:38:54 -0500

    add src/shared

--- cobot_linux/src/hardware/cobot3_drive/cobot_drive_main.cpp
+++ cobot_linux/src/hardware/cobot3_drive/cobot_drive_main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <stdio.h>
+#include <math.h> 
+#include <sys/time.h>
+
+#include "drive.h"              
+#include "terminal_utils.h"     
+#include "proghelp.h"           
+#include "configreader.h"
 
 int main(int argc, char **argv) {
     std::cout << "Cobot Drive Main Program" << std::endl;
